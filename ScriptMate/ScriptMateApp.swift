75ce4247eb7795579befe83b669d2572e0b3cc42
Charlie Loughran <charlie.loughran@outlook.com>
2023-05-07 20:09:53 +0100

    Initial Commit

---
+++ ScriptMate/ScriptMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScriptMateApp.swift
+//  ScriptMate
+//
+//  Created by Charlie Loughran on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScriptMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
